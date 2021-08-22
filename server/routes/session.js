dd6638420f337a58ff2bd37c89a1792ac370111d
Sabry6 <8914408@studenti.uniupo.it>
2021-08-22 16:11:10 +0000

    init6

--- server/routes/session.js
+++ server/routes/session.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const users = require('../users.json');
-const multipart = require('connect-multipart');
-const multipartMiddleware = multipart({uploadDir:'./uploads'});
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({ uploadDir:'./uploads'})
 
 router.post('/upload',multipartMiddleware,function(req,res,next){
-    res.send("File caricato!");
+    res.render('upload',{title:'Upload test',fileName: req.files.filetoupload.name});
+    //res.send("File caricato!");
 })
 
 router.get('/upload', function(req, res, next){
@@ -28,6 +29,20 @@ router.post('/', function(req, res, next) {
   
 });
 
+router.post('/', function(req, res, next) {
+    let username = req.body.username;
+    let email = req.body.email;
+    let psw = req.body.psw;
+    let rpsw = req.body.rpsw;
+
+    if(psw != rpsw){
+        res.send("<p style= \"color:red\"> Le password non coincidono!</p>")
+    } else{
+        res.send("<p style= \"color:green\"> Registrazione completata!</p>")
+    }
+  
+});
+
 /*router.get('/', function(req, res, next){
     let mail=req.query.user;
     let user = users.find(el=>el.email == mail); 
@@ -40,6 +55,14 @@ router.get('/:email/:color', function(req, res, next){
     let mail=req.params.email;
     let user = users.find(el=>el.email == mail); 
     user["color"]=req.params.color;
+    res.render('user',user);
+});
+
+router.get('/users', function(req,res,next){
+    res.render('users', {users: users});
+});
+router.get('/users/:email', function (req,res,next){
+    let user = users.find(u =>  u.email == req.params.email);
     res.render('user',user);
 });
 
